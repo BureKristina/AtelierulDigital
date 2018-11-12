7315b3e1c9a4c3f0ea4b4f41a20f1878b19b2880
BureKristina <tina.bure@gmail.com>
2018-11-12 12:59:47 +0200

    "first java commit"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,28 @@
 package com.company;
 
+import java.util.Scanner;
+
+class Numere {
+    private int[] numereCitite = new int[6];
+    private Scanner nou = new Scanner(System.in);
+
+    public Numere() {
+        for(int i = 0; i < numereCitite.length; i++) {
+            numereCitite[i] = nou.nextInt();
+        }
+    }
+
+    public void afisare(){
+        for(int i = 0; i < numereCitite.length; i++)
+            System.out.println("numereCitite[" + i + "] = " + numereCitite[i]);
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World!");
+        Numere n = new Numere();
+        n.afisare();
     }
 }
